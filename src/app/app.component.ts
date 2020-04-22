c871162f84071a8e9251d222e14a1bda676295eb
Alexander Caroselli <Alexander.caroselli@allata.com>
2020-04-22 17:13:35 -0600

    Initial generic dialog is now in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GenericDialogComponent } from './generic-dialog/generic-dialog.component';
+
+
+interface DialogConfiguration {
+  data: {
+    title:string;
+    content:string;
+    button1Text:string;
+    button2Text:string;
+    includeCheckbox?:boolean;
+  },
+  width: string
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +22,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'generic-dialog';
+  
+  //inject the MatDialog service
+  constructor(private dialog:MatDialog){
+  }
+  
+  
+  openModal() {
+
+    // let optionalConfigObject = {height:'400px', width:'600px'}
+    //use the MatDialog service to open the dialog by passing in the dialog component name
+    // and add optional config object if you like
+    
+
+    let dialogConfig: DialogConfiguration = 
+    { data:
+       { title:'Title',
+         content:'Cont en tad fadsfff ffff fffff ffffffff ffffffff fffffffffffff ffffffffff ffffffffffff fffffffffffff fffffffffffff ffffffffff ffffffff',
+         button1Text:'Update',
+         button2Text:'Cancel',
+         includeCheckbox:true
+       },
+       width:'400px'
+    }
+
+    //You can also pass in data 
+    this.dialog.open(GenericDialogComponent, dialogConfig)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GenericDialogComponent } from './generic-dialog/generic-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //Add dialog component name below
+    GenericDialogComponent
+  ],
+  entryComponents:[
+    //Also add dialog component name below
+    GenericDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //Add module below
+    MatDialogModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
